f14b00fd3b37f9fd5b4fb9e4e9a389db45159bae
Nicopee <gidudunicholas11@gmail.com>
2024-10-19 21:01:06 +0300

    Slight change

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      //home: const Overview(),
-      home: SignUpScreen(),
+      home: const Overview(),
+      //home: SignUpScreen(),
       //home: SplashScreen1(),
     );
   }
